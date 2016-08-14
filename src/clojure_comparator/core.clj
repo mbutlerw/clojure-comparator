af888f588dd738d041316fb1b6b2e3988a8ba915
Matt Butler <mattb-w@hotmail.co.uk>
2016-08-14 21:38:27 +0100

    adds collecting hiring partners

--- src/clojure_comparator/core.clj
+++ src/clojure_comparator/core.clj
@@ -5,18 +5,26 @@
 
 (defn -main
   [args]
-  (collect-companies 4))
+  (collect-companies))
 
 (defn string-trimmer [string]
-  (str/replace
-    (str/replace string "</a></p>" "") ">" ""))
+    (str/replace
+      (str/replace string "</a></p>" "") ">" ""))
 
-(defn collect-companies [arg]
-  (map string-trimmer
-    (re-seq  #">[\w| |.]+</a></p>"
-      (get
-        (str/split
-          (slurp "http://clojure.org/community/companies") #"ulist") 1)))
-)
+(defn collect-companies []
+  (str/lower-case
+    (into []
+      (map string-trimmer
+        (re-seq  #">[\w| |.]+</a></p>"
+          (get
+            (str/split
+              (slurp "http://clojure.org/community/companies") #"ulist") 1))))))
 
-(collect-companies 4)
+(defn collect-hiring-partners []
+  (str/lower-case
+    (str/split
+      (slurp "doc/hiring_partners.txt") #"\n")))
+
+(filter #(= (count %) 1)
+(collect-companies)
+(collect-hiring-partners)
