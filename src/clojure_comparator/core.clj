f73f2b39072c00ecdf64015236f4ee50de803394
Matt Butler <mattb-w@hotmail.co.uk>
2016-08-14 02:31:34 +0100

    adds collecting sources

--- src/clojure_comparator/core.clj
+++ src/clojure_comparator/core.clj
@@ -5,16 +5,18 @@
 
 (defn -main
   [args]
-  (map string-trimmer
-    (re-seq  #">[\w| |.]+</a></p>"
-      (get
-        (str/split
-          (slurp "http://clojure.org/community/companies") #"ulist") 1))))
+  (collect-companies 4))
 
 (defn string-trimmer [string]
   (str/replace
     (str/replace string "</a></p>" "") ">" ""))
 
-(defn collect-companies [args]
-
+(defn collect-companies [arg]
+  (map string-trimmer
+    (re-seq  #">[\w| |.]+</a></p>"
+      (get
+        (str/split
+          (slurp "http://clojure.org/community/companies") #"ulist") 1)))
 )
+
+(collect-companies 4)
